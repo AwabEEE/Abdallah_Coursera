2a63988a989b04619207a5bd7d85ce9b23ed7adc
AwabEEE <awab.milan@hotmail.com>
2025-04-06 15:39:48 +0300

    Added function prototypes and comments for all functions

--- stats.c
+++ stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief < a simple application that performs statistical analytics on a dataset >
  *
- * <Add Extended Description Here>
+ * <    Write a simple programming assignment that prints statistics to the command line using a variety of c-programming operators and features.>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Awab Abdallah>
+ * @date <6 april 2025 >
  *
  */
 
@@ -35,9 +35,87 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+    print_array(test, SIZE);
+
+// After sorting the array
+ printf("\nSorted Array (Largest to Smallest):\n");
+sort_array(test, SIZE);
+
+// Print the sorted array
+
+	print_array(test, SIZE);
+
+    /* Print the statistics */
+    print_statistics(test, SIZE);
 
 }
 
 /* Add other Implementation File Code Here */
+/* Find Maximum */
+unsigned char find_maximum(unsigned char *array, unsigned int length) {
+    unsigned char max = array[0];
+    for (unsigned int i = 1; i < length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+
+unsigned char find_minimum(unsigned char *array, unsigned int length) {
+    unsigned char min = array[0];
+    for (unsigned int i = 1; i < length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+unsigned char find_mean(unsigned char *array, unsigned int length) {
+    unsigned int sum = 0;
+    for (unsigned int i = 0; i < length; i++) {
+        sum += array[i];
+    }
+    return sum / length;
+}
+unsigned char find_median(unsigned char *array, unsigned int length) {
+    sort_array(array, length); // Sort array first
+
+    if (length % 2 == 0) {
+        // Even number of elements, average the two middle values
+        unsigned char mid1 = array[length / 2 - 1];
+        unsigned char mid2 = array[length / 2];
+        return (mid1 + mid2) / 2;
+    } else {
+        // Odd number of elements, return the middle value
+        return array[length / 2];
+    }
+}
+void sort_array(unsigned char *array, unsigned int length) {
+    for (unsigned int i = 0; i < length - 1; i++) {
+        for (unsigned int j = 0; j < length - i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                // Swap the values if they're in the wrong order
+                unsigned char temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+void print_array(unsigned char *array, unsigned int length) {
+    printf("Array values: ");
+    for (unsigned int i = 0; i < length; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+void print_statistics(unsigned char *array, unsigned int length) {
+    printf("Array Statistics:\n");
+    printf("Maximum: %d\n", find_maximum(array, length));
+    printf("Minimum: %d\n", find_minimum(array, length));
+    printf("Mean: %d\n", find_mean(array, length));
+    printf("Median: %d\n", find_median(array, length));
+}
--- stats.h
+++ stats.h
@@ -7,34 +7,101 @@
  * software. Alex Fosdick and the University of Colorado are not liable for any
  * misuse of this material. 
  *
- *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ *****************************************************************************//**
+ * @file <stats.h> 
+ * @brief <the header file , function prototype >
  *
- * <Add Extended Description Here>
+ * <The file declares the functions that will be implemented in the stats.c>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Awab Abdallh>
+ * @date <6 april 2025 >
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/* Function Declarations and Comments */
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Finds the maximum value in an array.
  *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * This function iterates through the array and finds the largest value
+ * by comparing each element. It returns the maximum value found
+ * @return The maximum value in the array.
  */
+unsigned char find_maximum(unsigned char *array, unsigned int length);
 
+/**
+ * @brief Finds the minimum value in an array.
+ *
+ * This function iterates through the array and finds the smallest value
+ * by comparing each element. It returns the minimum value found.
+ *
+ */
+unsigned char find_minimum(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Calculates the mean (average) value of an array.
+ * This function sums all the elements of the array and then divides by
+ * the number of elements to calculate the mean
+ * @param array A pointer to the unsigned char array that holds the data.
+ * @param length The number of elements in the array.
+ * @return The mean value of the array
+
+*/
+
+unsigned char find_mean(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Finds the median value of an array.
+ *
+ * This function first sorts the array and then finds the median value.
+ * If the number of elements is odd, the middle value is returned.
+ * If even, the average of the two middle values is returned.
+ *
+ * @param array A pointer to the unsigned char array that holds the data.
+@param length The number of elements in the array.
+ *
+ * @return The median value of the array.
+ */
+unsigned char find_median(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Sorts an array from largest to smallest.
+ *
+ * This function uses a sorting algorithm to sort the elements of the array
+ * in descending order.
+ *
+ * @param array A pointer to the unsigned char array that holds the data.
+ * @param length The number of elements in the array.
+ *
+ * @return void This function doesn't return any value.
+ */
+void sort_array(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Prints all elements of the array.
+ *
+ * This function iterates over the array and prints each element to the screen.
+ *
+ * @param array A pointer to the unsigned char array that holds the data.
+ * @param length The number of elements in the array.
+ *
+ * @return void This function doesn't return any value.
+ */
+void print_array(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Prints statistics such as maximum, minimum, mean, and median.
+ *
+ * This function calculates the statistics (max, min, mean, median) and 
+ * prints them to the screen.
+ *
+ * @param array A pointer to the unsigned char array that holds the data.
+ * @param length The number of elements in the array.
+ *
+ * @return void This function doesn't return any value.
+ */
+void print_statistics(unsigned char *array, unsigned int length);
 
 #endif /* __STATS_H__ */
